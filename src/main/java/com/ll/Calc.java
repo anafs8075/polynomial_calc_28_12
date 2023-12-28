ce9c084b0a9a9076a2ccd6ec3c04106c36ecb6b7
anafs8075 <anafs8075@gmail.com>
2023-12-29 04:26:16 +0900

    10 * 10 * 10

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,23 +2,35 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+    boolean needToMultiplay = exp.contains("*");
+    boolean needToPlus = !needToMultiplay;
+
+    if (needToPlus) {
+      exp = exp.replaceAll("\\- ", "\\+ \\-");
 
 
-    exp = exp.replaceAll("\\- ", "\\+ \\-");
+      String[] bits = exp.split(" \\+ ");
 
+      int sum = 0;
 
-    String[] bits = exp.split(" \\+ ");
+      for (int i = 0; i < bits.length; i++) {
+        sum += Integer.parseInt(bits[i]);
+      }
 
-    int sum = 0;
+      return sum;
+    } else if (needToMultiplay) {
+      String[] bits = exp.split(" \\* ");
 
-    for (int i = 0; i < bits.length; i++) {
-      sum += Integer.parseInt(bits[i]);
+      int rs = 1;
+
+      for (int i = 0; i < bits.length; i++) {
+        rs *= Integer.parseInt(bits[i]);
+      }
+      return rs;
     }
 
-    return sum;
-
-
-//    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+      throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
+
 
